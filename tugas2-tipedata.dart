1836588feb5b7423bbc00dd2ca41c543832baa3e
GrivineFilbert <grivinef@gmail.com>
2026-09-07 20:57:16 +0700

    versi2

--- tugas2-tipedata.dart
+++ tugas2-tipedata.dart
@@ -1,8 +1,13 @@
 void  main () {
-int a = 1;
-double b = 2;
-String c = "3";
-bool d = true;
-num e = 4;
-print(a + b + double.parse(c) + (d ? 1 : 0) + e);
+// int a = 1;
+// double b = 2;
+// String c = "3";
+// bool d = true;
+// num e = 4;
+// print(a + b + double.parse(c) + (d ? 1 : 0) + e);\
+
+
+const int x = 200;
+print(3.14 * x * x);
+print(2 * 3.14 * x);    
 }
